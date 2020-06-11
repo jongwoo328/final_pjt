42105bf9f129a5e8b20fb5a880a7f4b93a17bde8
jongwoo <jongwoo328@gmail.com>
2020-06-11 15:02:46 +0900

    0611 accounts view

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model, login as auth_login, logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
+User = get_user_model()
+
+@login_required
 def profile(request):
-    pass
+    user = request.user
+    context = {
+        'user': user,
+    }
+    return render(request, 'accounts/profile.html', context)
 
 def login(request):
-    pass
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('community:community')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/form.html', context)
 
 def signup(request):
-    pass
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('community:community')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/form.html', context)
 
+@login_required
 def logout(request):
-    pass
+    auth_logout(request)
+    return redirect('community:community')
 
+@login_required
 def update(request):
-    pass
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:profile')
+    else:
+        form = CustomUserChangeForm()
+    conetxt = {
+        'form': form,
+    }
+    return render(request, 'accounts/form.html', context)
 
+@login_required
 def delete(request):
-    pass
+    user = request.user
+    user.delete()
+    return redirect('community:community')
