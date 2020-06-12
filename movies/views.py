7985865acc33aa90d3b8b0a8b79416d4f4808082
jykim <jiyoung112837@gmail.com>
2020-06-12 13:49:17 +0900

    200612(jykim) - Modify movies/index.html

--- movies/views.py
+++ movies/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
+import datetime
 
 from .models import Movie, Genre, Review
 from .forms import ReviewForm
