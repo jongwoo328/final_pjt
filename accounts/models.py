94086652d5456d4a7e8c726ea7423d21b5e5a67f
jongwoo <jongwoo328@gmail.com>
2020-06-12 17:42:34 +0900

    0612 comment like view function

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-from community.models import Article
+from community.models import Article, Comment
 from movies.models import Movie
 
 
 class CustomUser(AbstractUser):
     liked_articles = models.ManyToManyField(Article, related_name='liked_users')
     liked_movies = models.ManyToManyField(Movie, related_name='liked_users')
+    liked_comments = models.ManyToManyField(Comment, related_name='liked_users')
