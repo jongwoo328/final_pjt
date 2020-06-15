e59721bd7aa49cbaf10eaf0c919a7c83ff65e38f
jongwoo <jongwoo328@gmail.com>
2020-06-15 13:13:01 +0900

    0615 noimage border gray

--- movies/views.py
+++ movies/views.py
@@ -119,7 +119,7 @@ def index(request, sort=None):
         'page_obj': page_obj,
         'nowDate': nowDate,
     }
-    print(page_number, type(page_number))
+    
     if not page_number or int(page_number) == 1 : return render(request, 'movies/index.html', context)
     else : return render(request, 'movies/index_scroll.html', context)
     
